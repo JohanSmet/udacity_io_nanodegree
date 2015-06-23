6a3019202c10bcae4427d5505f14c35fedb3d9db
Johan Smet <johan.smet@justcode.be>
2015-06-23 22:09:48 +0200

    On The Map : added MKMapView to the map-tab

---
+++ On The Map/On The Map/MapTabController.swift
@@ -0,0 +1,32 @@
+//
+//  MapTabController.swift
+//  On The Map
+//
+//  Created by Johan Smet on 23/06/15.
+//  Copyright (c) 2015 Justcode.be. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class MapTabController : UIViewController {
+   
+    ///////////////////////////////////////////////////////////////////////////////////
+    //
+    // outlets
+    //
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    ///////////////////////////////////////////////////////////////////////////////////
+    //
+    // UIViewController overrides
+    //
+    
+    ///////////////////////////////////////////////////////////////////////////////////
+    //
+    // actions
+    //
+    
+}
