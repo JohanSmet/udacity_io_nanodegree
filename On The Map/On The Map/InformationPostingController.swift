7a2b6e474ea9bfc5bb25db3828d8ab5bb209f4f3
Johan Smet <johan.smet@justcode.be>
2015-06-30 22:49:13 +0200

    On The Map : basic functionality of the information posting screen (without submitting)

--- On The Map/On The Map/InformationPostingController.swift
+++ On The Map/On The Map/InformationPostingController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Foundation
+import MapKit
 
 class InformationPostingController: UIViewController {
     
@@ -19,6 +20,21 @@ class InformationPostingController: UIViewController {
     @IBOutlet weak var containerLocation: UIView!
     @IBOutlet weak var containerLink: UIView!
     
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var locationText: UITextView!
+    @IBOutlet weak var linkText: UITextView!
+    
+    @IBOutlet weak var buttonSubmit: UIButton!
+    @IBOutlet weak var buttonFindOnMap: UIButton!
+    
+    ///////////////////////////////////////////////////////////////////////////////////
+    //
+    // variables
+    //
+    
+    private var locationCoordinate : CLLocationCoordinate2D?
+    
     ///////////////////////////////////////////////////////////////////////////////////
     //
     // UIViewController overrides
@@ -26,6 +42,15 @@ class InformationPostingController: UIViewController {
     
     override func viewDidLoad() {
         
+        // UI tweaks
+        buttonSubmit.layer.cornerRadius = 5
+        buttonFindOnMap.layer.cornerRadius = 5
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        // make sure to show the correct view
+        containerLocation.hidden = false
+        containerLink.hidden = true
     }
     
     ///////////////////////////////////////////////////////////////////////////////////
@@ -42,12 +67,74 @@ class InformationPostingController: UIViewController {
     }
     
     @IBAction func findOnMap(sender: AnyObject) {
-        containerLocation.hidden = true
-        containerLink.hidden = false
+        
+        // did the user enter a location ?
+        if locationText.text!.isEmpty {
+            alert("You must enter a location before you can continue!", title: "Attention")
+            return
+        }
+        
+        // geocode
+        geocodeLocation(locationText.text!) { error in
+            
+            if let error = error {
+                self.alert(error.localizedDescription)
+            } else {
+                // show the entered location on the map
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = self.locationCoordinate!
+                self.mapView.addAnnotation(annotation)
+                
+                // center on te location
+                self.mapView.setRegion(MKCoordinateRegion(center: self.locationCoordinate!, span: MKCoordinateSpanMake(0.01, 0.01)), animated: true)
+                
+                // switch to the next view
+                self.containerLocation.hidden = true
+                self.containerLink.hidden = false
+            }
+        }
     }
 
     @IBAction func submitLocation(sender: AnyObject) {
+       
+        // did the user enter a link
+        if linkText.text!.isEmpty {
+            alert("You must enter a link before you can continue!", title: "Attention")
+            return
+        }
+       
+        // submit the information to udacity
+        
     }
+    
+    ///////////////////////////////////////////////////////////////////////////////////
+    //
+    // helper functions
+    //
+    
+    private func geocodeLocation(location : String, completionHandler: (error : NSError?) -> Void) {
+        
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(location, completionHandler: { (placemarks : [AnyObject]!, error : NSError?) in
+            if let error = error {
+                completionHandler(error: error)
+            } else {
+                let placemark = placemarks[0] as! CLPlacemark
+                self.locationCoordinate = placemark.location.coordinate
+                completionHandler(error: nil)
+            }
+        })
+    }
+    
+    private func alert(message : String, title : String = "Alert") {
+        dispatch_async(dispatch_get_main_queue(), {
+            var alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        })
+    }
+    
     
 }
 
