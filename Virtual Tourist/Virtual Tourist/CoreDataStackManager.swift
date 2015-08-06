ae45f3e7cba3bcb27ef5f9fd4d067a421314beb8
Johan Smet <johan.smet@justcode.be>
2015-08-06 22:02:08 +0200

    Virtual Tourist : basic project structure

---
+++ Virtual Tourist/Virtual Tourist/CoreDataStackManager.swift
@@ -0,0 +1,92 @@
+//
+//  CoreDataStackManager.swift
+//  Virtual Tourist
+//
+//  Created by Johan Smet on 06/08/15.
+//  Copyright (c) 2015 Justcode.be. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+private let SQLITE_FILE_NAME = "VirtualTourist.sqlite"
+private let MODEL_NAME = "VirtualTourist"
+
+class CoreDataStackManager {
+    
+    lazy var applicationDocumentsDirectory: NSURL = {
+        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        return urls[urls.count-1] as! NSURL
+    }()
+    
+    lazy var managedObjectModel: NSManagedObjectModel = {
+        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
+        let modelURL = NSBundle.mainBundle().URLForResource(MODEL_NAME, withExtension: "momd")!
+        return NSManagedObjectModel(contentsOfURL: modelURL)!
+    }()
+    
+    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
+        // Create the coordinator and store
+        var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent(SQLITE_FILE_NAME)
+        
+        var error: NSError? = nil
+        var failureReason = "There was an error creating or loading the application's saved data."
+        
+        if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil, error: &error) == nil {
+            coordinator = nil
+            // Report any error we got.
+            var dict = [String: AnyObject]()
+            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
+            dict[NSLocalizedFailureReasonErrorKey] = failureReason
+            dict[NSUnderlyingErrorKey] = error
+            error = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+            // Replace this with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog("Unresolved error \(error), \(error!.userInfo)")
+            abort()
+        }
+        
+        return coordinator
+    }()
+    
+    lazy var managedObjectContext: NSManagedObjectContext? = {
+        let coordinator = self.persistentStoreCoordinator
+        if coordinator == nil {
+            return nil
+        }
+        var managedObjectContext = NSManagedObjectContext()
+        managedObjectContext.persistentStoreCoordinator = coordinator
+        return managedObjectContext
+    }()
+    
+    func saveContext () {
+        if let moc = self.managedObjectContext {
+            var error: NSError? = nil
+            if moc.hasChanges && !moc.save(&error) {
+                // Replace this implementation with code to handle the error appropriately.
+                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                NSLog("Unresolved error \(error), \(error!.userInfo)")
+                abort()
+            }
+        }
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////////
+    //
+    // singleton
+    //
+    
+    class func sharedInstance() -> CoreDataStackManager {
+        struct Static {
+            static let instance = CoreDataStackManager()
+        }
+        
+        return Static.instance
+    }
+}
+
+// shorthand way to git the CoreDataStackManager shared instance
+func coreDataStackManager() -> CoreDataStackManager {
+    return CoreDataStackManager.sharedInstance()
+}
---
+++ Virtual Tourist/Virtual Tourist/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewController.swift
+//  Virtual Tourist
+//
+//  Created by Johan Smet on 06/08/15.
+//  Copyright (c) 2015 Justcode.be. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
